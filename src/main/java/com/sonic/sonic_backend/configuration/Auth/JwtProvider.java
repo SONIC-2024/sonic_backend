24f1e04d4c4b12dfacb9ab604855bfa256b5cb1f
gol2580 <gol11971197@gmail.com>
2024-08-14 18:06:40 +0900

    [fix] update access token expire time

--- src/main/java/com/sonic/sonic_backend/configuration/Auth/JwtProvider.java
+++ src/main/java/com/sonic/sonic_backend/configuration/Auth/JwtProvider.java
@@ -22,8 +22,8 @@ import java.util.stream.Collectors;
 @Component
 public class JwtProvider {
     //access token 유효기간
-    private final long EXPIRE_TIME_ACCESS = 1000 * 60 * 30;
-    private final long EXPIRE_TIME_REFRESH = 1000 * 60 * 60 * 24 * 7;
+    private final long EXPIRE_TIME_ACCESS = 1000 * 60 * 60; //1시간
+    private final long EXPIRE_TIME_REFRESH = 1000 * 60 * 60 * 24 * 7; //1주일
     private final Key key;
     public JwtProvider(@Value("${jwt.secret}") String secretKey) {
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
